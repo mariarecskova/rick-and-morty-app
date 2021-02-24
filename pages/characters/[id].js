8f0a54d58318662cfde394fbff55129fa4f470c4
Maria <maria.recska@gmail.com>
2021-02-24 18:56:22 +0100

    links between episodes and characters is working

--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,11 +1,11 @@
- import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
- import Image from "next/image";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import Image from "next/image";
+import Link from "next/link";
 
- 
- export async function getStaticPaths() {
+export async function getStaticPaths() {
   const client = new ApolloClient({
     uri: "https://rickandmortyapi.com/graphql/",
-    cache: new InMemoryCache(), 
+    cache: new InMemoryCache(),
   });
 
   const { data } = await client.query({
@@ -15,30 +15,30 @@
           results {
             name
             id
+          }
         }
       }
-    }
-    `, 
+    `,
   });
-  const paths = data.characters.results.map(character => {
-      return {
-          params: {id: character.id.toString() }
-      }
-  })
+  const paths = data.characters.results.map((character) => {
+    return {
+      params: { id: character.id.toString() },
+    };
+  });
   return {
-      paths: paths,
-      fallback: false
-    }
-  }
+    paths: paths,
+    fallback: false,
+  };
+}
 
 export const getStaticProps = async (context) => {
-    const id = context.params.id;
-    const client = new ApolloClient({
-      uri: "https://rickandmortyapi.com/graphql/",
-      cache: new InMemoryCache(),
-    });
-    const { data } = await client.query({
-      query: gql`
+  const id = context.params.id;
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql/",
+    cache: new InMemoryCache(),
+  });
+  const { data } = await client.query({
+    query: gql`
         query {
           character(id: ${id}) {
             name
@@ -47,24 +47,41 @@ export const getStaticProps = async (context) => {
               name
             }
             species
+            episode {
+                id
+                name
+                episode
+            }
           }
         }
       `,
-    });
+  });
 
-    return {
-        props: {
-          character: data.character,
-        },
-      };
-}
-export default function Details({character}) {
-    return (
-      <div>
-        <h2>{character.name}</h2>
-        <p>{character.location.name}</p>
-        <p>{character.species}</p>
-        <Image src={character.image} width={300} height={300} padding={30} />
-      </div>
-    );
+  return {
+    props: {
+      character: data.character,
+    },
+  };
+};
+export default function Details({ character }) {
+  return (
+    <div>
+      <h2>{character.name}</h2>
+      <p>{character.location.name}</p>
+      <p>{character.species}</p>
+      <Image src={character.image} width={300} height={300} padding={30} />
+      <ul style={{ listStyle: "none" }}>
+        {character.episode.map((episode) => {
+          return (
+            <li>
+              <Link href={"/episodes/" + episode.id} key={episode.id}>
+                <a>{episode.name}</a>
+              </Link>
+              <p>{episode.episode}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- pages/episodes/[id].js
+++ pages/episodes/[id].js
@@ -1,3 +1,81 @@
-export default function Details() {
-  return <h2>Character Details</h2>;
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import Image from "next/image";
+import Link from "next/link";
+
+export async function getStaticPaths() {
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql/",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query {
+        episodes {
+          results {
+            name
+            id
+          }
+        }
+      }
+    `,
+  });
+  const paths = data.episodes.results.map((episode) => {
+    return {
+      params: { id: episode.id.toString() },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql/",
+    cache: new InMemoryCache(),
+  });
+  const { data } = await client.query({
+    query: gql`
+      query {
+        episode(id: ${id}) {
+          name
+          episode
+          air_date
+          characters {
+            id
+            name
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      episode: data.episode,
+    },
+  };
+};
+export default function Details({ episode }) {
+  return (
+    <div>
+      <h2>{episode.name}</h2>
+      <p>{episode.episode}</p>
+      <p>{episode.air_date}</p>
+      <ul style={{ listStyle: "none" }}>
+        {episode.characters.map((character) => {
+          return (
+            <li>
+              <Link href={"/characters/" + character.id} key={character.id}>
+                <a>{character.name}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }

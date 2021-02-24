655c487ed7ae335fc1e79b352227438a96f0cf74
Maria <maria.recska@gmail.com>
2021-02-24 18:03:24 +0100

    dynamic page generation works with REST

--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,4 +1,40 @@
-export default function Details() {
-   
-    return <h2>Character Details</h2>
-}
+ import Image from "next/image";
+
+ export const getStaticPaths = async () => {
+   const res = await fetch("https://rickandmortyapi.com/api/character");
+   const data = await res.json();
+
+   const paths = data.results.map((character) => {
+     return {
+       params: { id: character.id.toString() },
+     };
+   });
+
+   return {
+     paths,
+     fallback: false,
+   };
+ };
+
+ export const getStaticProps = async (context) => {
+   const id = context.params.id;
+   const res = await fetch("https://rickandmortyapi.com/api/character/" + id);
+   const data = await res.json();
+
+   return {
+     props: { character: data },
+   };
+ };
+
+ const Details = ({ character }) => {
+   return (
+     <div>
+       <h1>{character.name}</h1>
+       <p>{character.species}</p>
+       <p>{character.location.name}</p>
+       <Image src={character.image} width={300} height={300} padding={30} />
+     </div>
+   );
+ };
+
+ export default Details;

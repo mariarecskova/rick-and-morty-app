0d8bcf9a0d388aa8357c7e2663aafb4379ba2658
Maria <maria.recska@gmail.com>
2021-02-24 20:05:21 +0100

    index and character page styled

--- pages/characters.js
+++ pages/characters.js
@@ -2,8 +2,27 @@ import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image"
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 // import Link from "next/link";
+
+const useStyles = makeStyles((theme) => ({
+  
+  root: {
+    maxWidth: 300,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "30px",
+    margin: "auto"
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+}));
 
 export async function getStaticProps() {
   const client = new ApolloClient({
@@ -42,25 +61,39 @@ export async function getStaticProps() {
 
 
 export default function CharacterOverview({ characters }) {
+   const classes = useStyles();
   return (
     <div>
       <Head></Head>
       <div>
-        <h1>Rick and Morty Characters</h1>
+        <h1 style={{margin: "auto", textAlign: "center", padding: "20px"}} >
+         Rick and Morty Characters
+        </h1>
         <ul style={{ listStyle: "none" }}>
           {characters.map((character) => {
             return (
-              <li>
-                <Link href={"/characters/" + character.id} key={character.id}>
-                  <a>{character.name}</a>
-                </Link>
-                <Image
-                  src={character.image}
-                  width={300}
-                  height={300}
-                  padding={20}
-                />
-              </li>
+              <Card className={classes.root}>
+                <li>
+                  <CardMedia>
+                    <Image
+                      src={character.image}
+                      width={300}
+                      height={300}
+                      padding={20}
+                    />
+                  </CardMedia>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    <Link
+                      href={"/characters/" + character.id}
+                      key={character.id}
+                    >
+                      <a style={{ textDecoration: "none", color: "black" }}>
+                        {character.name}
+                      </a>
+                    </Link>
+                  </Typography>
+                </li>
+              </Card>
             );
           })}
         </ul>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,47 @@
 import Link from "next/link";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Home() {
+  const classes = useStyles();
+
   return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Rick and Morty Application</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/characters">
-          <a>Characters</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/episodes">
-          <a>Episodes</a>
-        </Link>
-      </li>
-    </ul>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Rick and Morty Application
+          </Typography>
+          <Button color="inherit">
+            <Link href="/characters">
+              <a style={{ textDecoration: "none", color: "white" }}>
+                Characters
+              </a>
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link href="/episodes">
+              <a style={{ textDecoration: "none", color: "white" }}>Episodes</a>
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
